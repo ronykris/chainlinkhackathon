dd966628ee8614dd7225d729b909c948f633df56
ronykris <rony.kris@gmail.com>
2022-11-18 22:49:19 +0530

    code cleanup

--- debookmark-build/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-    purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-    content: [],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
-}
